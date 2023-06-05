f40ff73ee7405076e1124dc9bbfe4eb9c630b6aa
Gabriel Taborda Farias <gabrielfariasbass@gmail.com>
2023-06-05 11:28:18 -0300

    adding new constants for modbus

--- src/consts/modbus.ts
+++ src/consts/modbus.ts
@@ -6,3 +6,13 @@ export const VALID_FUNCTION_CODES: ModbusReadFunctions[] = [
   "03",
   "04",
 ];
+
+/**
+ * Lowest valid value of the Modbus register address range.
+ */
+export const LOWEST_VALUE_REGISTER_ADDRES_RANGE = 0;
+
+/**
+ * Highest valid value of the Modbus register address range.
+ */
+export const HIGHEST_VALUE_REGISTER_ADDRES_RANGE = 65535;
