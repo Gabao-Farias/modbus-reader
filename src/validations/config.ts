37ecfb534c49fc913dd42b90b0acc9dfd82e5972
Gabriel Taborda Farias <gabrielfariasbass@gmail.com>
2023-06-05 00:47:49 -0300

    refactoring validations to receive unknown data

--- src/validations/config.ts
+++ src/validations/config.ts
@@ -1,9 +1,11 @@
 import { CRON_VALIDATION_REGEX, VALID_BAUD_RATES } from "../consts";
 
 /**
- * Checks if a given baud rate is valid.
+ * Checks if a given data is a valid baud rate.
  */
-export const isValidBaudRate = (baudRate: string): boolean => {
+export const isValidBaudRate = (baudRate: unknown): boolean => {
+  if (typeof baudRate !== "string") return false;
+
   for (let index = 0; index < VALID_BAUD_RATES.length; index++) {
     const validBaudRate = VALID_BAUD_RATES[index];
 
@@ -14,12 +16,15 @@ export const isValidBaudRate = (baudRate: string): boolean => {
 };
 
 /**
- * Checks if the given string is a valid cron format.
+ * Checks if the given data is a valid cron format.
  * @param cron
  * @returns
  */
-export const isValidCron = (cron: string): boolean =>
-  CRON_VALIDATION_REGEX.test(cron);
+export const isValidCron = (cron: unknown): boolean => {
+  if (typeof cron !== "string") return false;
+
+  return CRON_VALIDATION_REGEX.test(cron);
+};
 
 /**
  * Checks if the given data is a valid Modbus slave address.
