58179d43192d5b967b0c24fe183c8336a46d969b
Gabriel Taborda Farias <gabrielfariasbass@gmail.com>
2023-06-05 00:41:30 -0300

    adding new validations functions for config

--- src/validations/config.ts
+++ src/validations/config.ts
@@ -20,3 +20,16 @@ export const isValidBaudRate = (baudRate: string): boolean => {
  */
 export const isValidCron = (cron: string): boolean =>
   CRON_VALIDATION_REGEX.test(cron);
+
+/**
+ * Checks if the given data is a valid Modbus slave address.
+ * @param cron
+ * @returns
+ */
+export const isValidSlaveID = (sid: unknown): boolean => {
+  if (typeof sid !== "number") return false;
+
+  if (sid < 1 || sid > 247) return false;
+
+  return true;
+};
