ad13387389455af0a2e404247dcda04286efe2d2
Gabriel Taborda Farias <gabrielfariasbass@gmail.com>
2023-06-05 00:18:47 -0300

    adding tests for validations functions

--- src/validations/config.test.ts
+++ src/validations/config.test.ts
@@ -1,5 +1,6 @@
 import * as Config from "./config";
-const { isValidBaudRate } = jest.requireActual<typeof Config>("./config");
+const { isValidBaudRate, isValidCron } =
+  jest.requireActual<typeof Config>("./config");
 
 describe("Config validation test suite", () => {
   test("[isValidBaudRate] 1. When a baud rate is valid, it should return true", () => {
@@ -12,5 +13,35 @@ describe("Config validation test suite", () => {
     const invalidBaudRate = "111111";
 
     expect(isValidBaudRate(invalidBaudRate)).toBe(false);
+  });
+
+  const validCron1 = "*/5 * * * *";
+  test(`[isValidCron] 1. When a cron is valid (${validCron1}), it should return true`, () => {
+    expect(isValidCron(validCron1)).toBe(true);
+  });
+
+  const validCron2 = "5-30 * * * *";
+  test(`[isValidCron] 2. When a cron is valid (${validCron2}), it should return true`, () => {
+    expect(isValidCron(validCron2)).toBe(true);
+  });
+
+  const validCron3 = "*/6 */6 */6 */6 */6";
+  test(`[isValidCron] 3. When a cron is valid (${validCron3}), it should return true`, () => {
+    expect(isValidCron(validCron3)).toBe(true);
+  });
+
+  const invalidCron1 = "*6 6 */6 *6 6";
+  test(`[isValidCron] 4. When a cron is invalid (${invalidCron1}), it should return false`, () => {
+    expect(isValidCron(invalidCron1)).toBe(false);
+  });
+
+  const invalidCron2 = "* * 68 * *";
+  test(`[isValidCron] 5. When a cron is invalid (${invalidCron2}), it should return false`, () => {
+    expect(isValidCron(invalidCron2)).toBe(false);
+  });
+
+  const invalidCron3 = "* * * * 10";
+  test(`[isValidCron] 6. When a cron is invalid (${invalidCron3}), it should return false`, () => {
+    expect(isValidCron(invalidCron3)).toBe(false);
   });
 });
