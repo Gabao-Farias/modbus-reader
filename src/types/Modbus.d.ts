67904140afd16bc1d258e7606425d70059de48eb
Gabriel Taborda Farias <gabrielfariasbass@gmail.com>
2023-06-06 11:33:40 -0300

    adding new types for modbus

--- src/types/Modbus.d.ts
+++ src/types/Modbus.d.ts
@@ -4,6 +4,30 @@
 declare type ModbusReadFunctions = "01" | "02" | "03" | "04";
 
 /**
+ * Modbus functions available for use.
+ */
+declare type ModbusReadFunctionsNames =
+  | "readCoils"
+  | "readDiscreteInputs"
+  | "readHoldingRegisters"
+  | "readInputRegisters";
+
+/**
  * Modbus baud rates available for use.
  */
 declare type BaudRates = "9600" | "115200";
+
+/**
+ * Modbus props for read purpose.
+ */
+declare type ModbusReadProps = {
+  address: string;
+  functionCode: ModbusReadFunctions;
+};
+
+/**
+ * Generic mapper for modbus read functions.
+ */
+declare type ModbusReadFunctionsGenericMapper<T = ModbusReadFunctionsNames> = {
+  [x in ModbusReadFunctions]: T;
+};
